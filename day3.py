59db6edd3ae938d11aa0227e83307f326c19ea46
dr_njitram <martijn.gobes@gmail.com>
2019-12-03 17:29:52 +0100

    day 3

--- day3.py
+++ day3.py
@@ -7,20 +7,18 @@ def get_all_points(path):
     points = set()
     dist = dict()
     new_pos = (0, 0)
-    next_step = (0, 0)
+
     steps = 0
 
     for instruction in path:
         direct = {"U": (0, 1), "D": (0, -1), "L": (-1, 0), "R": (1, 0)}[instruction[0]]
         length = int(instruction[1:])
 
-        for i in range(length):
-            next_step = new_pos[0] + i * direct[0], new_pos[1] + i * direct[1]
-            points.update(tuple([next_step]))
-            dist[next_step] = steps + i
+        next_steps = [(new_pos[0] + i * direct[0], new_pos[1] + i * direct[1]) for i in range(1, length + 1)]
+        points.update(next_steps)
+        dist.update([(next_steps[i], i + 1 + steps) for i in range(length)])
 
-
-        new_pos = next_step
+        new_pos = next_steps[-1]
         steps += length
 
     return points, dist
@@ -31,11 +29,24 @@ def solution2():
 
     crossings = points & points_2
 
-    crossings_length = [abs(cr[0]) + abs(cr[1]) for cr in crossings]
-    costs = [dist_1[cr] + dist_2[cr] for cr in crossings]
+    crossings_length = sorted([abs(cr[0]) + abs(cr[1]) for cr in crossings])[0]
+    costs = sorted([dist_1[cr] + dist_2[cr] for cr in crossings])[0]
 
-    #print("Part 1:", sorted(crossings_length)[0])
-    #print("Part 2:", sorted(costs)[0])
+
+def solution2_print():
+    points, dist_1 = get_all_points(wires[0])
+    points_2, dist_2 = get_all_points(wires[1])
+
+    crossings = points & points_2
+
+    crossings_length = sorted([abs(cr[0]) + abs(cr[1]) for cr in crossings])
+    costs = sorted([dist_1[cr] + dist_2[cr] for cr in crossings])
+
+    print("Part 1:", crossings_length[0])
+    print("Part 2:", costs[0])
+
+
+solution2_print()
 
 reps = 100
 print(timeit(solution2, number=reps)/reps)
