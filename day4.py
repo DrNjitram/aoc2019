fd19019175cb54aaa9f50fe6e3353be975a6ebb9
dr_njitram <martijn.gobes@gmail.com>
2019-12-04 17:59:28 +0100

    to get nixx to stop complaining

--- day4.py
+++ day4.py
@@ -14,19 +14,30 @@ def solution():
     end_no = 579381
     started = False
 
+
+    start_arr = [start[0]] + [-1 if j < start[i - 1] else j for i, j in enumerate(start[1:])]
+    start_arr = [i for i in start_arr if i > -1]
+    start_arr = start_arr + [start_arr[-1]]
+
+    end_arr = [end[0]] + [-1 if j < end[i - 1] else j for i, j in enumerate(end[1:])]
+    end_arr = [i for i in end_arr if i > -1]
+    end_arr = end_arr + [end_arr[-1]]
+
+
+
     for a in range(start[0], end[0] + 1):
         for b in range(a, 10):
             for c in range(b, 10):
                 for d in range(c, 10):
                     for e in range(d, 10):
                         for f in range(e, 10):
-                            if started or [a, b, c, d, e] == [1, 2, 5, 7, 7]:
+                            if started or [a, b, c, d, e] == start_arr:
                                 started = True
                                 result = check_number([a, b, c, d, e, f])
                                 amounts_1 += result[0]
                                 amounts_2 += result[1]
 
-                                if [a, b, c, d] == [5, 7, 9, 9]:
+                                if [a, b, c, d] == end_arr:
                                     #print("Part 1:", amounts_2)
                                     #print("Part 2:", amounts_1)
                                     return 0
