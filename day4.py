f434b1f5ec6bc1417efcbd32b0e5ead91a87b34c
dr_njitram <martijn.gobes@gmail.com>
2019-12-04 16:14:32 +0100

    stupid fast

--- day4.py
+++ day4.py
@@ -1,16 +1,35 @@
 def check_number(str_no):
-    if str_no[0] <= str_no[1] <= str_no[2] <= str_no[3] <= str_no[4] <= str_no[5]:
-        if str_no[0] == str_no[1] != str_no[2] or str_no[0] != str_no[1] == str_no[2] != str_no[3] or str_no[1] != str_no[2] == str_no[3] != str_no[4] or str_no[2] != str_no[3] == str_no[4] != str_no[5] or str_no[3] !=  str_no[4] == str_no[5]:
-            return 0, 1
-        elif str_no[0] == str_no[1] or str_no[1] == str_no[2] or str_no[2] == str_no[3] or str_no[3] == str_no[4] or str_no[4] == str_no[5]:
-            return 1, 0
-    return 0, 0
+    if str_no[0] == str_no[1] != str_no[2] or str_no[0] != str_no[1] == str_no[2] != str_no[3] or str_no[1] != str_no[2] == str_no[3] != str_no[4] or str_no[2] != str_no[3] == str_no[4] != str_no[5] or str_no[3] !=  str_no[4] == str_no[5]:
+        return 0, 1
+    elif str_no[0] == str_no[1] or str_no[1] == str_no[2] or str_no[2] == str_no[3] or str_no[3] == str_no[4] or str_no[4] == str_no[5]:
+        return 1, 0
+    else:
+        return 0, 0
 
-amount_1, amount_2 = 0, 0
-for i in range(125730, 579381 + 1):
-    result = check_number(str(i))
-    amount_1 += result[0]
-    amount_2 += result[1]
+def solution():
+    amounts_1, amounts_2 = 0, 0
+    start = [1,2,5,7,3,0]
+    end = [5,7,9,3,8,1]
+    start_no = 125730
+    end_no = 579381
 
-print("Part 1:", amount_2)
-print("Part 2:", amount_2 + amount_1)
+    for a in range(start[0], end[0] + 1):
+        for b in range(a, 10):
+            for c in range(b, 10):
+                for d in range(c, 10):
+                    for e in range(d, 10):
+                        for f in range(e, 10):
+                            if a*10**5 + b*10**4 + c*10**3 + d * 10**2 + e * 10 + f > start_no:
+                                result = check_number([a, b, c, d, e, f])
+                                amounts_1 += result[0]
+                                amounts_2 += result[1]
+
+                                if a*10**5 + b*10**4 + c*10**3 + d * 10**2 + e * 10 + f > end_no:
+                                    #print("Part 1:", amounts_2)
+                                    #print("Part 2:", amounts_2 + amounts_1)
+                                    return 0
+
+
+from timeit import timeit
+print(timeit(solution, number=10)/10)
+
